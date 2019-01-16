8b171298494e6ab750ff576a5e2953612ece1dbf
Markes105 <rubendegroot@msn.com>
2019-01-16 14:59:33 +0100

    idk

--- testsearch.py
+++ testsearch.py
@@ -5,6 +5,8 @@ import pprint as pp
 t = requests.get("http://api.yummly.com/v1/api/recipes?_app_id=6553a906&_app_key=21ef3e857585ece9f97b0831c08af72e&allowedAllergy[]=393^Gluten-Free")
 x = json.loads(t.text)
 
-for i in range(len(x["matches"])):
-    pp.pprint(x["matches"][i])
-    print()
+# for i in range(len(x["matches"])):
+#     pp.pprint(x["matches"][i])
+#     print()
+
+print(x)
