d8f4d45fee9b29bd084f265c340a1e4615a0140f
Andeshhh <46585918+Andeshhh@users.noreply.github.com>
2019-01-16 15:13:53 +0100

    nothing special

--- application.py
+++ application.py
@@ -25,7 +25,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # configure CS50 Library to use SQLite database
-db = SQL("sqlite:///users.db")
+db = SQL("sqlite:///recepts.db")
 
 @app.route("/")
 @app.route("/index")
