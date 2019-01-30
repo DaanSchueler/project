d2c3b670c8588adc8fb47a366529ceebf570e91e
Markes105 <rubendegroot@msn.com>
2019-01-30 14:43:47 +0100

    Javascript netjes gemaakt

--- static/script.js
+++ static/script.js
@@ -1,62 +1,58 @@
-// function wantinfo(id) {
-//     console.log("more info wanted")
-//     $.ajax({
-//         url: '/moreinfo',
-//         data: {"id" : id},
-//     });
-// }
+// Function for like buttons
+function like() {
 
-function like(){
-    $(".like_button").click(function(event) {
- // / 1. form get value
- // / 2. Ajax call /addtodb + data=this.value()
- // / 3. addtodb vraag waarde op args.get
- // / 4. database.query
+  // Listens for click on the like button
+  $(".like_button").click(function(event) {
 
- var fired_button = $(this).val();
- var endpoint = "";
- if ($(event.target).html() == 'Like'){
-   endpoint=  "/like"
- }
- else{
-   endpoint = "/unlike"
- }
- $.ajax({
-  type : 'POST',
-  url : endpoint,
-  data : JSON.stringify({fired_button}, null, '\t'),
-  contentType: 'application/json;charset=UTF-8',
-  success: function() 
-  {
-      if ($(event.target).html() == 'Like'){
-        $(event.target).html("Unlike");
-    }
-    else{
+      // Make variable to save value button
+      var fired_button = $(this).val();
 
-      $(event.target).html("Like");
-    }
- }
- });
+      // Create url endpoint
+      var endpoint = "";
+
+      //Change endpoint if button == like or unlike
+      if ($(event.target).html() == 'Like') {
+          endpoint = "/like"
+      } else {
+          endpoint = "/unlike"
+      }
+
+      //Ajax post request to respective approute 
+      $.ajax({
+          type: 'POST',
+          url: endpoint,
+          data: JSON.stringify({
+              fired_button
+          }, null, '\t'),
+          contentType: 'application/json;charset=UTF-8',
+
+          // If succesvol change text like button to like/unlike
+          success: function() {
+              if ($(event.target).html() == 'Like') {
+                  $(event.target).html("Unlike");
+              } else {
+
+                  $(event.target).html("Like");
+              }
+          }
+      });
 
 
-})
+  })
 }
 
+// Function for like buton if not logged in
 
-function cannot(){
-    $(".like_button").click(function(event) {
-     
-    document.getElementById("alert").style.display = "block";     
-       
-       })
-       }
+function cannot() {
+  $(".like_button").click(function(event) {
 
+      document.getElementById("alert").style.display = "block";
 
-   $("#likerfield" ).change(function() {
-    $('#find_user').prop('disabled', false);
-    });
-   
-  
+  })
+}
 
+// Disable find user at more-info.html
 
-    
+$("#likerfield").change(function() {
+  $('#find_user').prop('disabled', false);
+});
